7b4ee6db059fa94fddc7ece505e185222d447bc4
tedkatapetrov1 <teodor.petrov@abv.bg>
2025-07-02 15:46:31 +0300

    removing questions from script.js and adding them to questions.js

---
+++ Quizz app/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+      question: "Коя е столицата на България?",
+      answers: [
+        { text: "София", correct: true },
+        { text: "Пловдив", correct: false },
+        { text: "Варна", correct: false },
+        { text: "Русе", correct: false }
+      ]
+    },
+    {
+      question: "Кой е написал 'Под игото'?",
+      answers: [
+        { text: "Елин Пелин", correct: false },
+        { text: "Иван Вазов", correct: true },
+        { text: "Христо Ботев", correct: false },
+        { text: "Пейо Яворов", correct: false }
+      ]
+    },
+    {
+      question: "Кой е най-големият океан?",
+      answers: [
+        { text: "Атлантическият", correct: false },
+        { text: "Тихият", correct: true },
+        { text: "Индийският", correct: false },
+        { text: "Северният ледовит", correct: false }
+      ]
+    }
+  ];
--- Quizz app/script.js
+++ Quizz app/script.js
@@ -1,33 +1,3 @@
-const questions = [
-    {
-      question: "Коя е столицата на България?",
-      answers: [
-        { text: "София", correct: true },
-        { text: "Пловдив", correct: false },
-        { text: "Варна", correct: false },
-        { text: "Русе", correct: false }
-      ]
-    },
-    {
-      question: "Кой е написал 'Под игото'?",
-      answers: [
-        { text: "Елин Пелин", correct: false },
-        { text: "Иван Вазов", correct: true },
-        { text: "Христо Ботев", correct: false },
-        { text: "Пейо Яворов", correct: false }
-      ]
-    },
-    {
-      question: "Кой е най-големият океан?",
-      answers: [
-        { text: "Атлантическият", correct: false },
-        { text: "Тихият", correct: true },
-        { text: "Индийският", correct: false },
-        { text: "Северният ледовит", correct: false }
-      ]
-    }
-  ];
-  
   const questionContainer = document.getElementById('question-container');
   const questionElement = document.getElementById('question');
   const answerButtons = document.getElementById('answer-buttons');
