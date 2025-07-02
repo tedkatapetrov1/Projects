8e1d981ead9e86845dd791c4202a596152da2e17
tedkatapetrov1 <teodor.petrov@abv.bg>
2025-07-02 16:16:06 +0300

    new button for starting the quiz and js function for starting the quiz again

--- Quizz app/script.js
+++ Quizz app/script.js
@@ -1,90 +1,99 @@
-  const questionContainer = document.getElementById('question-container');
-  const questionElement = document.getElementById('question');
-  const answerButtons = document.getElementById('answer-buttons');
-  const nextBtn = document.getElementById('next-btn');
-  const resultContainer = document.getElementById('result-container');
-  const scoreText = document.getElementById('score');
-  const totalText = document.getElementById('total');
-  
-  let currentQuestionIndex = 0;
-  let score = 0;
-  
-  function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    resultContainer.classList.add('hide');
-    nextBtn.innerText = "Следващ въпрос";
-    showQuestion();
-  }
-  
-  function showQuestion() {
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    questionElement.innerText = currentQuestion.question;
-  
-    currentQuestion.answers.forEach(answer => {
-      const btn = document.createElement('button');
-      btn.innerText = answer.text;
-      btn.classList.add('btn');
-      if (answer.correct) {
-        btn.dataset.correct = answer.correct;
-      }
-      btn.addEventListener('click', selectAnswer);
-      answerButtons.appendChild(btn);
-    });
-  }
-  
-  function resetState() {
-    nextBtn.style.display = 'none';
-    while (answerButtons.firstChild) {
-      answerButtons.removeChild(answerButtons.firstChild);
-    }
-  }
-  
-  function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const correct = selectedBtn.dataset.correct === "true";
-  
-    if (correct) score++;
-  
-    Array.from(answerButtons.children).forEach(button => {
-      setStatusClass(button, button.dataset.correct === "true");
-      button.disabled = true;
-    });
-  
-    nextBtn.style.display = 'block';
-  }
-  
-  function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-      element.classList.add('correct');
-    } else {
-      element.classList.add('wrong');
-    }
-  }
-  
-  function clearStatusClass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
-  }
-  
-  nextBtn.addEventListener('click', () => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-      showQuestion();
-    } else {
-      showResult();
-    }
-  });
-  
-  function showResult() {
-    questionContainer.classList.add('hide');
-    nextBtn.style.display = 'none';
-    resultContainer.classList.remove('hide');
-    scoreText.innerText = score;
-    totalText.innerText = questions.length;
-  }
-  
+const startBtn = document.getElementById('start-btn');
+const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-buttons');
+const nextBtn = document.getElementById('next-btn');
+const resultContainer = document.getElementById('result-container');
+const scoreText = document.getElementById('score');
+const totalText = document.getElementById('total');
+const restartBtn = document.getElementById('restart-btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+startBtn.addEventListener('click', () => {
+  startBtn.classList.add('hide');
+  questionContainer.classList.remove('hide');
+  nextBtn.classList.add('hide');
+  resultContainer.classList.add('hide');
   startQuiz();
-  
+});
+
+restartBtn.addEventListener('click', () => {
+    resultContainer.classList.add('hide');
+    startBtn.classList.remove('hide');
+});
+
+nextBtn.addEventListener('click', () => {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showResult();
+  }
+});
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  const currentQuestion = questions[currentQuestionIndex];
+  questionElement.innerText = currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const btn = document.createElement('button');
+    btn.innerText = answer.text;
+    btn.classList.add('btn');
+    if (answer.correct) {
+      btn.dataset.correct = "true";
+    }
+    btn.addEventListener('click', selectAnswer);
+    answerButtons.appendChild(btn);
+  });
+}
+
+function resetState() {
+  nextBtn.classList.add('hide');
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const correct = selectedBtn.dataset.correct === "true";
+  if (correct) score++;
+
+  Array.from(answerButtons.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct === "true");
+    button.disabled = true;
+  });
+
+  nextBtn.classList.remove('hide');
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+function showResult() {
+  questionContainer.classList.add('hide');
+  nextBtn.classList.add('hide');
+  resultContainer.classList.remove('hide');
+  scoreText.innerText = score;
+  totalText.innerText = questions.length;
+}
